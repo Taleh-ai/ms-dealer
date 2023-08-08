476fcdc5e07c606c4438e3da19247b8f53adb030
TalehRza <58006591+Taleh-ai@users.noreply.github.com>
2023-08-08 10:44:28 +0400

    Update ProductEntity.java
    product entity

--- src/main/java/com/example/msdealer/entity/ProductEntity.java
+++ src/main/java/com/example/msdealer/entity/ProductEntity.java
@@ -1,4 +1,17 @@
 package com.example.msdealer.entity;
 
+@Entity
+@Table(name = "products")
 public class ProductEntity {
+  @Id
+  Long id;
+  String name;
+  String brand;
+  String colour;
+  String size;
+  String description;
+  Long dealerId;
+  Long category_id;
+  int quantity;
+  
 }
